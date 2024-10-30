d9699d46b7c143d200ddc8eead14643b48390c1b
cs-mg-gh <145803911+cs-mg-gh@users.noreply.github.com>
2024-10-30 13:58:45 -0700

    Initial commit

---
+++ src/main/java/com/csumb/otterpicks/OtterPicksApplication.java
@@ -0,0 +1,13 @@
+package com.csumb.otterpicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterPicksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtterPicksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csumb/otterpicks/OtterPicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csumb.otterpicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtterPicksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
